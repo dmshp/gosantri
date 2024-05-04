0b21444e0f152fa271f135aa84bbb37e69c109eb
ToniNugrahaPutra <80298868+ToniNugrahaPutra@users.noreply.github.com>
2024-05-04 17:58:28 +0700

    update history

--- go/p/master/history.php
+++ go/p/master/history.php
@@ -1,157 +1,157 @@
-<div class="card mt-2"> 
-  <div class="content-wrapper">
-   <div class="content-detached content-right">
-    <div class="content-body">
-	 <div class="divider">
-		<div class="divider-text">Riwayat Transaksi</div>
-	 </div>
-                    
-<div class="shop-content-overlay"></div>
-<!-- Centered Pills Start -->
- <section id="centered-pills"> 
-           <ul class="nav nav-pills nav-active-bordered-pill justify-content-center">
-             <li class="nav-item mb-1 mr-1">
-               <a class="nav-link active btn btn-icon rounded-0" id="all-tab" data-toggle="pill" href="#all-center" aria-expanded="true"><i class="fa-solid fa-clipboard-check"></i></a>
-             </li>
-             <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="unpaid-tab" data-toggle="pill" href="#unpaid" aria-expanded="false"><i class="fa-solid fa-sack-xmark"></i></a>
-             </li>			   
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="cek-tab" data-toggle="pill" href="#cek" aria-expanded="false"><i class="fa-solid fa-user-check"></i></a>
-             </li>
-			<li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="pack-tab" data-toggle="pill" href="#pack" aria-expanded="false"><i class="fa-solid fa-boxes-packing"></i></a>
-             </li>
-			<li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="find-tab" data-toggle="pill" href="#find" aria-expanded="false"><i class="fa-solid fa-magnifying-glass-location"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="pr-tab" data-toggle="pill" href="#pr" aria-expanded="false"><i class="fa-solid fa-truck-ramp-box"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="send-tab" data-toggle="pill" href="#send" aria-expanded="false"><i class="fa-solid fa-truck-fast"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="acc-tab" data-toggle="pill" href="#acc" aria-expanded="false"><i class="fa-solid fa-hand-holding-heart"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="finish-tab" data-toggle="pill" href="#finish" aria-expanded="false"><i class="fa-regular fa-handshake"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="cancel-tab" data-toggle="pill" href="#cancel" aria-expanded="false"><i class="fa-regular fa-circle-xmark"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="pending-tab" data-toggle="pill" href="#pending" aria-expanded="false"><i class="fa-solid fa-handshake-simple-slash"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="cash-tab" data-toggle="pill" href="#cash" aria-expanded="false"><i class="fa-solid fa-check-to-slot"></i></a>
-             </li>
-			 <li class="nav-item mb-1 mr-1">
-                <a class="nav-link btn btn-icon rounded-0" id="reject-tab" data-toggle="pill" href="#reject" aria-expanded="false"><i class="fa-solid fa-ban"></i></a>
-             </li>
-             
-           </ul>
-          <hr class="my-1"> 
-		  <div class="tab-content m-0 p-0">
-           
-			<div role="tabpanel" class="tab-pane active" id="all-center" aria-labelledby="all-tab" aria-expanded="true"> 
-                 
-                   <div class="card-header">
-                      <h4 class="card-title">Semua Riwayat</h4>
-                   </div>
-                       <div class="table-responsive">
-                         <table class="table nowrap scroll-horizontal-vertical">
+<style type="text/css">
+   .dataTables_scrollHeadInner,
+   div.dataTables_scrollHead table.dataTable,
+   div.dataTables_scrollBody table {
+      width: 100% !important;
+   }
+</style>
+<div class="card mt-2 mx-auto">
+   <div class="content-wrapper">
+      <div class="content-detached content-right">
+         <div class="content-body">
+            <div class="divider">
+               <div class="divider-text">Riwayat Transaksi</div>
+            </div>
+
+            <div class="shop-content-overlay"></div>
+            <!-- Centered Pills Start -->
+            <section id="centered-pills">
+               <ul class="nav nav-pills nav-active-bordered-pill justify-content-center">
+                  <li class="nav-item  m-1">
+                     <a class="nav-link active btn btn-icon rounded-0" id="all-tab" data-toggle="pill"
+                        href="#all-center" aria-expanded="true"><i class="fa-solid fa-clipboard-check"></i></a>
+                  </li>
+                  <?php
+                  $ketQuery = "SELECT * FROM tabel_pembelian_status";
+                  $executeSat = mysqli_query($koneksi, $ketQuery);
+                  $statusOrder = [];
+                  while ($b = mysqli_fetch_array($executeSat)) {
+
+                     ?>
+                     <li class="nav-item  m-1">
+                        <a class="nav-link btn btn-icon rounded-0" id="<?php echo $b['kd_pembelian_status']; ?>-tab"
+                           data-toggle="pill" href="#<?php echo $b['kd_pembelian_status']; ?>" aria-expanded="false"><i
+                              class="<?php echo $b['icon']; ?>"></i></a>
+                     </li>
+
+                     <?php
+                  }
+                  ?>
+
+               </ul>
+               <hr class="my-1">
+               <div class="tab-content m-0 p-0">
+                  <div role="tabpanel" class="tab-pane active" id="all-center" aria-labelledby="all-tab"
+                     aria-expanded="true">
+
+                     <div class="card-header">
+                        <h4 class="card-title">Semua Riwayat</h4>
+                     </div>
+                     <div class="table-responsive">
+                        <table class="table nowrap scroll-horizontal-vertical">
                            <thead>
-                             <tr>
-                                <th class="font-small-1">Faktur</th>
-                                <th class="font-small-1">Nominal</th>
-                                <th class="font-small-1">Ket</th>
-                                <th class="font-small-1">Status</th>
-                             </tr>
-                          </thead>
-                          <tbody>
-                             <tr>
-                               <td class="font-small-3">
-								   <b>NO.FAKTUR</b><br>
-								 <p class="text-italic font-small-1"><i class="fa-regular fa-clock"></i> Tgl/bulan/tahun : jam</p>
-							  </td>
-                               <td class="font-small-1"><sup>Rp.</sup> 20.000</td>
-                               <td class="font-small-1">Keterangan</td>
-                               <td class="font-small-1">Status</td>
-                             </tr> 
-                          </tbody>
-                          <tfoot>
-                             <tr>
-                               <th class="font-small-1">Faktur</th>
-                                <th class="font-small-1">Status</th>
-                                <th class="font-small-1">Ket</th>
-                                <th class="font-small-1">Cek</th>
-                             </tr>
-                          </tfoot>
+                              <tr>
+                                 <th class="font-small-1">Faktur</th>
+                                 <th class="font-small-1">Nominal</th>
+                                 <th class="font-small-1">Status</th>
+                              </tr>
+                           </thead>
+                           <tbody>
+                              <?php
+                              $id_user = $_SESSION['id_user'];
+                              $ketQuery = "SELECT * FROM tabel_pembelian as p, tabel_pembelian_status as ps, tabel_member as a WHERE p.status = ps.kd_pembelian_status and p.id_user = a.id_user and a.id_user = '$id_user' order by p.tgl_pembelian desc";
+                              $executeSat = mysqli_query($koneksi, $ketQuery);
+                              while ($b = mysqli_fetch_array($executeSat)) {
+                                 $no_faktur = isset($b['no_faktur_pembelian']) ? $b['no_faktur_pembelian'] : '';
+
+
+                                 ?>
+                                 <tr>
+                                    <td class="font-small-3">
+                                       <strong>
+                                          <?= $b['no_faktur_pembelian'] ?>
+                                       </strong><br>
+                                       <p class="text-italic font-small-1"><i class="fa-regular fa-clock"></i>
+                                          <?= $b['tgl_pembelian'] ?>
+                                       </p>
+                                    </td>
+                                    <td class="font-small-1"><sup>Rp. </sup>
+                                       <?= number_format($b['total_biaya'], 0, ",", "."); ?>
+                                    </td>
+                                    <td class="font-small-1">
+                                       <?= $b['nm_pembelian_status']; ?>
+                                    </td>
+                                 </tr>
+                              <?php } ?>
+                           </tbody>
                         </table>
-                      </div>
-            </div><!-- TAB ALL -->
-            
-			<div class="tab-pane" id="unpaid" role="tabpanel" aria-labelledby="unpaid-tab" aria-expanded="false">
-               <p>Belum Bayar</p>
-            </div>
-			  
-			<div class="tab-pane" id="cek" role="tabpanel" aria-labelledby="cek-tab" aria-expanded="false">
-               <p>Pengecekkan Oleh Admin</p>
-            </div>
-			  
-			<div class="tab-pane" id="pack" role="tabpanel" aria-labelledby="pack-tab" aria-expanded="false">
-               <p>Pengemasan</p>
-            </div>
-			  
-			<div class="tab-pane" id="find" role="tabpanel" aria-labelledby="find-tab" aria-expanded="false">
-               <p>Pencarian Kurir</p>
-            </div>
-			  
-			<div class="tab-pane" id="pr" role="tabpanel" aria-labelledby="pr-tab" aria-expanded="false">
-               <p>Pengambilan paket</p>
-            </div>
-			  
-			<div class="tab-pane" id="send" role="tabpanel" aria-labelledby="send-tab" aria-expanded="false">
-               <p>Paket Dikirim</p>
-            </div>
-			  
-			<div class="tab-pane" id="acc" role="tabpanel" aria-labelledby="acc-tab" aria-expanded="false">
-               <p>Paket Diterima</p>
-            </div>
-			  
-			<div class="tab-pane" id="finish" role="tabpanel" aria-labelledby="finish-tab" aria-expanded="false">
-               <p>Transaksi selesai</p>
-            </div>
-			  
-			<div class="tab-pane" id="cancel" role="tabpanel" aria-labelledby="cancel-tab" aria-expanded="false">
-               <p>Paket Dibatalkan</p>
-            </div>
-			  
-			<div class="tab-pane" id="pending" role="tabpanel" aria-labelledby="pending-tab" aria-expanded="false">
-               <p>Pembayaran ditangguhkan</p>
-            </div>
-			  
-			<div class="tab-pane" id="cash" role="tabpanel" aria-labelledby="cash-tab" aria-expanded="false">
-               <p>Pembayaran lunas</p>
-            </div>
-			  
-			<div class="tab-pane" id="reject" role="tabpanel" aria-labelledby="reject-tab" aria-expanded="false">
-               <p>Pembayaran ditolak</p>
-            </div>  
-            
-			
-			  
+                     </div>
+                  </div><!-- TAB ALL -->
+
+                  <?php
+                  $ketQueryc = "SELECT * FROM tabel_pembelian_status";
+                  $executeSatc = mysqli_query($koneksi, $ketQueryc);
+                  while ($bc = mysqli_fetch_array($executeSatc)) {
+
+                     ?>
+                     <div class="tab-pane" id="<?php echo $bc['kd_pembelian_status']; ?>" role="tabpanel"
+                        aria-labelledby="<?php echo $bc['kd_pembelian_status']; ?>-tab" aria-expanded="false">
+
+                        <div class="card-header">
+                           <h4 class="card-title">
+                              <?php echo $bc['nm_pembelian_status']; ?>
+                           </h4>
+                        </div>
+                        <div class="table-responsive">
+                           <table class="table nowrap scroll-horizontal-vertical">
+                              <thead>
+                                 <tr>
+                                    <th class="font-small-1">Faktur</th>
+                                    <th class="font-small-1">Nominal</th>
+                                    <th class="font-small-1">Status</th>
+                                 </tr>
+                              </thead>
+                              <tbody>
+                                 <?php
+                                 $status = $bc['kd_pembelian_status'];
+
+                                 $id_user = $_SESSION['id_user'];
+                                 $ketQuery = "SELECT * FROM tabel_pembelian as p, tabel_pembelian_status as ps, tabel_member as a WHERE p.status = ps.kd_pembelian_status and p.id_user = a.id_user and a.id_user = '$id_user' and p.status='$status' order by p.tgl_pembelian desc";
+                                 $executeSat = mysqli_query($koneksi, $ketQuery);
+                                 while ($b = mysqli_fetch_array($executeSat)) {
+                                    $no_faktur = isset($b['no_faktur_pembelian']) ? $b['no_faktur_pembelian'] : '';
+
+
+                                    ?>
+                                    <tr>
+                                       <td class="font-small-3">
+                                          <strong>
+                                             <?= $b['no_faktur_pembelian'] ?>
+                                          </strong><br>
+                                          <p class="text-italic font-small-1"><i class="fa-regular fa-clock"></i>
+                                             <?= $b['tgl_pembelian'] ?>
+                                          </p>
+                                       </td>
+                                       <td class="font-small-1"><sup>Rp. </sup>
+                                          <?= number_format($b['total_biaya'], 0, ",", "."); ?>
+                                       </td>
+                                       <td class="font-small-1">
+                                          <?= $b['nm_pembelian_status']; ?>
+                                       </td>
+                                    </tr>
+                                 <?php } ?>
+                              </tbody>
+                           </table>
+                        </div>
+
+                     </div>
+                  <?php } ?>
+
+               </div>
+
+            </section>
+            <!-- Centered Pills End -->
          </div>
-      
-     
- </section>
-<!-- Centered Pills End -->
-		
-		
-    
- 
+      </div>
    </div>
-  </div>
- </div>
 </div>
